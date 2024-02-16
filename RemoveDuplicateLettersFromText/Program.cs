8ee1bc769937985765ac3b0004ca7e98f0eca654
Rashad Yusifli <rashad_y@bestcomp.net>
2024-02-16 10:07:42 +0400

    1

---
+++ RemoveDuplicateLettersFromText/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+const string text = "aabbbacdeeef";
+
+Console.WriteLine(text[2..3]);
+
+if (text.Length > 0)
+{
+    StringBuilder sb = new(text[0].ToString());
+
+    foreach (char letter in text)
+    {
+        if (sb[^1] != letter)
+            sb.Append(letter);
+    }
+
+    Console.WriteLine(sb.ToString());
+    Console.ReadLine();
+}
