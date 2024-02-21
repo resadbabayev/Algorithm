bdf10ce6dfeb67e6615138e78bb3e4573cca8781
Rashad Yusifli <rashad_y@bestcomp.net>
2024-02-21 09:49:10 +0400

    1

---
+++ FindSamePrefixes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+
+string[] array = { "flower", "floor", "flight" };
+
+StringBuilder sb = new ();
+
+int minLength = array[0].Length;
+
+for (int i = 0; i < minLength; i++)
+{
+    char currentChar = array[0][i];
+    bool allSame = true;
+
+    for (int j = 1; j < array.Length; j++)
+    {
+        if (array[j][i] != currentChar)
+        {
+            allSame = false;
+            break;
+        }
+    }
+
+    if (allSame)
+    {
+        sb.Append(currentChar);
+    }
+    else
+    {
+        break;
+    }
+}
+
+Console.WriteLine(sb.ToString());
+Console.ReadLine();
