ddb31e28606a863056d329b0ac9f531196a6bb45
Rashad Yusifli <rashad_y@bestcomp.net>
2024-02-21 12:42:51 +0400

    1

--- RomanNumeralConverter/Program.cs
+++ RomanNumeralConverter/Program.cs
@@ -1,25 +1,57 @@
-﻿string input = "MMCCXXXIX"; // 2239
+﻿namespace RomanNumeralConverter;
 
-Dictionary<char, int> dictionary = new()
+public static class Program
+{
+    static readonly Dictionary<char, int> romanDict = new()
     {
-        { 'I', 1 }, { 'V', 5 }, { 'X', 10 }, { 'L', 50 },
-        { 'C', 100 }, { 'D', 500 }, { 'M', 1000 }
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
     };
 
-int result = 0;
-int lastValue = 0;
+    public static void Main()
+    {
+        Console.WriteLine(RomanToInt_V1("MMCCXXXIX")); // 2239
+        Console.WriteLine(RomanToInt_V2("MMCCXXXIX")); // 2239
+    }
 
-foreach (char symbol in input)
-{
-    int currentValue = dictionary[symbol];
+    public static int RomanToInt_V1(string s)
+    {
+        int romanSum = 0;
+        int lastValue = 0;
+        for (var i = 0; i < s.Length; i++)
+        {
+            int currentValue = romanDict[s[i]];
+            if (currentValue > lastValue)
+                romanSum += currentValue - 2 * lastValue;
+            else
+                romanSum += currentValue;
 
-    if (currentValue > lastValue)
-        result += currentValue - 2 * lastValue;
-    else
-        result += currentValue;
+            lastValue = currentValue;
+        }
 
-    lastValue = currentValue;
+        return romanSum;
+    }
+
+    public static int RomanToInt_V2(string s)
+    {
+        int romanSum = 0;
+        int prevValue = 0;
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            int currentValue = romanDict[s[i]];
+            if (prevValue <= currentValue)
+                romanSum += currentValue;
+            else
+                romanSum -= currentValue;
+
+            prevValue = currentValue;
+        }
+
+        return romanSum;
+    }
 }
-
-Console.WriteLine(result);
-Console.ReadLine();
